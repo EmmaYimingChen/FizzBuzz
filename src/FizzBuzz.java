d21012c7ba75b98e6b7637a79bc36b2f9e20be8d
EmmaYimingChen <emmaymchen@outlook.com>
2024-09-09 18:34:44 -0400

    TUT changes2

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -15,7 +15,7 @@ class FizzBuzz {
         }
     }
 
-    private static int dofizzbuzz(int i) {
+    public static int dofizzbuzz(int i) {
         boolean divisibleBy3 = i % 3 == 0;
         boolean divisibleBy5 = i % 5 == 0;
 
